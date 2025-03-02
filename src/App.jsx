ed6acaff20c44cbb66d50019d33864ce4c06e0c8
Alexy33 <alexy.canu@epitech.eu>
2025-03-03 00:10:36 +0100

    [ADD] vercel analitics in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/sections/About";
 import Projects from "./components/sections/Projects";
 import Skills from "./components/sections/Skills";
 import Contact from "./components/sections/Contact";
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   const [activeSection, setActiveSection] = useState(null);
@@ -159,6 +160,7 @@ const App = () => {
         </div>
         {renderActiveSection()}
         {renderInstructions()}
+        <Analytics />
       </Suspense>
     </div>
   );
