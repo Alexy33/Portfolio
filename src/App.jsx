101ffee95537f3e998f9f2aaa4dfb7a8462e8536
Alexy33 <alexy.canu@epitech.eu>
2025-03-06 13:24:45 +0100

    [FIX] new version of my CV

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from "./components/sections/About";
 import Projects from "./components/sections/Projects";
 import Skills from "./components/sections/Skills";
 import Contact from "./components/sections/Contact";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   const [activeSection, setActiveSection] = useState(null);
